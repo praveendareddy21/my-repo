a0c79b33bd0ca55a21667afae0ae5d96bb74399e
praveendareddy21 <praveendareddy@gmail.com>
2013-12-13 15:15:37 +0530

    Create pynuance.py

---
+++ pynuance.py
@@ -0,0 +1,20 @@
+class A :
+    g = 2
+    def ses(se, f) :
+        print se.g
+        print f
+a=A()
+a.ses(34)
+
+
+
+
+class B :
+    g = 2
+    def ses( f) :
+        print f.g
+        print f
+b=B()
+b.ses()
+
+
